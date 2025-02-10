ddd8466eaada574bce470d7053acca8639a5d824
gsnunes47 <gustavompn06@gmail.com>
2025-02-10 08:57:13 -0300

    adiciona o tailwind nos projetos

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"], // Garante que o Tailwind leia seus arquivos HTML
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
